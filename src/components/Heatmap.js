8e4d10daab5d4d272ba89bea453c2f53a907fac3
Piyush Sikarwal <sikarwal.ps@gmail.com>
2018-10-07 13:56:15 +0530

    Update src/components/Heatmap.js

--- src/components/Heatmap.js
+++ src/components/Heatmap.js
@@ -15,15 +15,10 @@ import MarkerPurple from '../static/icons/marker-purple.png';
 
 const Heatmap = compose(
   withProps({
-    /**
-     * Note: create and replace your own key in the Google console.
-     * https://console.developers.google.com/apis/dashboard
-     * The key "AIzaSyBkNaAGLEVq0YLQMi-PYEMabFeREadYe1Q" can be ONLY used in this sandbox (no forked).
-     */
     googleMapURL:
       'https://maps.googleapis.com/maps/api/js?key=AIzaSyAulodcRp7D6_M_5qsQm32hppHb20VRFdk&v=3.exp&libraries=geometry,drawing,places',
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
+    containerElement: <div style={{ height: `720px` }} />,
     mapElement: <div style={{ height: `100%` }} />
   }),
   withScriptjs,
